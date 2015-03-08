0bfa72e1671794e193dd264943fe146b2fb694c3
jameshu2008 <jameshu2008@gmail.com>
2015-03-08 11:20:29 -0700

    added vim_add_submodules.sh

---
+++ .vim/bundle/vim_add_submodules.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+rm -Rf ack
+rm -Rf delimitMate
+rm -Rf emmet
+rm -Rf gundo
+rm -Rf nerdtree
+rm -Rf supertab
+rm -Rf tagbar
+rm -Rf tcomment
+rm -Rf theme_molokai
+rm -Rf theme_tomorrow
+rm -Rf theme_vim-colors-solarized
+rm -Rf airline
+rm -Rf fugitive
+rm -Rf repeat
+rm -Rf snipmate
+rm -Rf snipmate_tlib_vim
+rm -Rf snipmate_vim-addon-mw-utils
+rm -Rf snipmate_vim-snippets
+rm -Rf surround
+rm -Rf syntastic
+rm -Rf indent-guides
+
+git submodule add https://github.com/mileszs/ack.vim.git ack
+git submodule add https://github.com/Raimondi/delimitMate.git delimitMate
+git submodule add https://github.com/mattn/emmet-vim.git emmet
+git submodule add https://github.com/sjl/gundo.vim.git gundo
+git submodule add https://github.com/scrooloose/nerdtree.git nerdtree
+git submodule add https://github.com/ervandew/supertab.git supertab
+git submodule add https://github.com/majutsushi/tagbar.git tagbar
+git submodule add https://github.com/tomtom/tcomment_vim.git tcomment
+git submodule add https://github.com/tomasr/molokai.git theme_molokai
+git submodule add https://github.com/chriskempson/tomorrow-theme.git theme_tomorrow
+git submodule add https://github.com/altercation/vim-colors-solarized.git theme_vim
+git submodule add https://github.com/bling/vim-airline.git airline
+git submodule add https://github.com/tpope/vim-fugitive.git fugitive
+git submodule add https://github.com/tpope/vim-repeat repeat
+git submodule add https://github.com/garbas/vim-snipmate.git snipmate
+git submodule add https://github.com/tomtom/tlib_vim.git snipmate_tlib_vim
+git submodule add https://github.com/MarcWeber/vim-addon-mw-utils.git snipmate_vim
+git submodule add https://github.com/honza/vim-snippets.git snipmate_vim
+git submodule add https://github.com/tpope/vim-surround.git surround
+git submodule add https://github.com/scrooloose/syntastic.git syntastic
+git submodule add https://github.com/nathanaelkane/vim-indent-guides.git indent-guides
