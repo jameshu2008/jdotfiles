eae636bf87ac9ecea1ff6233b815964c8782d64b
jameshu2008 <jameshu2008@gmail.com>
2015-03-08 10:48:24 -0700

    removed all bundles, modidfied vim_clone.sh

--- .vim/bundle/vim_clone.sh
+++ .vim/bundle/vim_clone.sh
@@ -1,41 +1,41 @@
 #! /bin/bash
 
-rm -Ri ack.vim
+rm -Ri ack
 rm -Ri delimitMate
-rm -Ri emmet-vim
-rm -Ri gundo.vim
-rm -Ri  nerdtree
-rm -Ri  supertab
-rm -Ri  tagbar
-rm -Ri  tcomment_vim
-rm -Ri  theme_molokai
-rm -Ri  theme_tomorrow
-rm -Ri  theme_vim-colors-solarized
-rm -Ri  vim-airline
-rm -Ri  vim-fugitive
-rm -Ri  vim-repeat
-rm -Ri  vim-snipmate
-rm -Ri  vim-snipmate_tlib_vim
-rm -Ri  vim-snipmate_vim-addon-mw-utils
-rm -Ri  vim-snipmate_vim-snippets
-rm -Ri  vim-surround
+rm -Ri emmet
+rm -Ri gundo
+rm -Ri nerdtree
+rm -Ri supertab
+rm -Ri tagbar
+rm -Ri tcomment
+rm -Ri theme_molokai
+rm -Ri theme_tomorrow
+rm -Ri theme_vim-colors-solarized
+rm -Ri airline
+rm -Ri fugitive
+rm -Ri repeat
+rm -Ri snipmate
+rm -Ri snipmate_tlib_vim
+rm -Ri snipmate_vim-addon-mw-utils
+rm -Ri snipmate_vim-snippets
+rm -Ri surround
 
-git clone https://github.com/mileszs/ack.vim.git ack.vim
-git clone https://github.com/Raimondi/delimitMate.git delimitMate
-git clone https://github.com/mattn/emmet-vim.git emmet-vim
-git clone https://github.com/sjl/gundo.vim.gi://github.com/sjl/gundo.vim.git gundo.vim
-git clone https://github.com/scrooloose/nerdtree.git nerdtree
-git clone https://github.com/ervandew/supertab.git supertab
-git clone https://github.com/majutsushi/tagbar.git tagbar
-git clone https://github.com/tomtom/tcomment_vim.git tcomment_vim
-git clone https://github.com/tomasr/molokai.git theme_molokai
-git clone https://github.com/chriskempson/tomorrow-theme.git theme_tomorrow
-git clone https://github.com/altercation/vim-colors-solarized.git theme_vim-colors-solarized
-git clone https://github.com/bling/vim-airline.git vim-airline
-git clone https://github.com/tpope/vim-fugitive.git vim-fugitive
-git clone https://github.com/tpope/vim-repeat vim-repeat
-git clone https://github.com/garbas/vim-snipmate.git vim-snipmate
-git clone https://github.com/tomtom/tlib_vim.git vim-snipmate_tlib_vim
-git clone https://github.com/MarcWeber/vim-addon-mw-utils.git vim-snipmate_vim-addon-mw-utils
-git clone https://github.com/honza/vim-snippets.git vim-snipmate_vim-snippets
-git clone https://github.com/tpope/vim-surround.git vim-surround
+git submodlule add https://github.com/mileszs/ack.vim.git ack
+git submodlule add https://github.com/Raimondi/delimitMate.git delimitMate
+git submodlule add https://github.com/mattn/emmet-vim.git emmet
+git submodlule add https://github.com/sjl/gundo.vim.git gundo
+git submodlule add https://github.com/scrooloose/nerdtree.git nerdtree
+git submodlule add https://github.com/ervandew/supertab.git supertab
+git submodlule add https://github.com/majutsushi/tagbar.git tagbar
+git submodlule add https://github.com/tomtom/tcomment_vim.git tcomment_vim
+git submodlule add https://github.com/tomasr/molokai.git theme_molokai
+git submodlule add https://github.com/chriskempson/tomorrow-theme.git theme_tomorrow
+git submodlule add https://github.com/altercation/vim-colors-solarized.git theme_vim-colors-solarized
+git submodlule add https://github.com/bling/vim-airline.git airline
+git submodlule add https://github.com/tpope/vim-fugitive.git fugitive
+git submodlule add https://github.com/tpope/vim-repeat repeat
+git submodlule add https://github.com/garbas/vim-snipmate.git snipmate
+git submodlule add https://github.com/tomtom/tlib_vim.git snipmate_tlib_vim
+git submodlule add https://github.com/MarcWeber/vim-addon-mw-utils.git snipmate_vim-addon-mw-utils
+git submodlule add https://github.com/honza/vim-snippets.git snipmate_vim-snippets
+git submodlule add https://github.com/tpope/vim-surround.git surround
